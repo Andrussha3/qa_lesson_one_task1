6f3de5bb89aa6f9cbcd2902849f39e0701aa44d7
Andrussha3 <lupolenkoandre@gmail.com>
2025-08-15 17:17:17 +0300

    Update Main.java
    изменение название переменных

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
 
-        int ticket_price = 13_676; // стоимость билета
+        int ticketPrice = 13_676; // стоимость билета
         int bonus = 20; // за сколько потраченных рублей начислят бонус
-        int bonus_ball = ticket_price / bonus; // итоговый расчет начисленных бонусов
-        System.out.print(bonus_ball);
+        int bonusBall = ticketPrice / bonus; // итоговый расчет начисленных бонусов
+        System.out.print(bonusBall);
     }
 }
